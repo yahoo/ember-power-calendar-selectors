b06306fc426f109ef6f70ee900307e37452d937f
Alex Aralis <alex.aralis@oath.com>
2018-09-18 15:38:16 -0500

    test years single

--- tests/integration/components/power-calendar-years/single/component-test.js
+++ tests/integration/components/power-calendar-years/single/component-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { assertionInjector, assertionCleanup } from 'dummy/tests/assertions';
@@ -34,5 +35,34 @@ module('Integration | Component | power-calendar-years/single', function(hooks) 
       this.element.textContent.replace(/\s+/g, ' ').trim(), 
       '2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020'
     );
+  });
+
+  test('The format of the years can be changed passing `format="<format string>"`', async function(assert) {
+    this.calendar = calendar;
+    
+    await render(hbs`{{power-calendar-years/single calendar=calendar format=format}}`);
+    assert.equal(
+      this.element.querySelector('.ember-power-calendar-selector-year-grid').textContent.replace(/\s+/g, ' ').trim(), 
+      '2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020'
+    );
+    run(() => this.set('format', 'YY'));
+    assert.equal(
+      this.element.querySelector('.ember-power-calendar-selector-year-grid').textContent.replace(/\s+/g, ' ').trim(), 
+      '09 10 11 12 13 14 15 16 17 18 19 20'
+    );
+    run(() => this.set('format', 'YY MMM'));
+    assert.equal(
+      this.element.querySelector('.ember-power-calendar-selector-year-grid').textContent.replace(/\s+/g, ' ').trim(), 
+      '09 Jan 10 Jan 11 Jan 12 Jan 13 Jan 14 Jan 15 Jan 16 Jan 17 Jan 18 Jan 19 Jan 20 Jan'
+    );
+  });
+
+  test('It can receive `data-power-calendar-id` and it is bound to an attribute', async function(assert) {
+    assert.expect(1);
+    this.calendar = calendar;
+    await render(
+      hbs`{{power-calendar-years/single calendar=calendar data-power-calendar-id="foobar"}}`
+    );
+    assert.dom('.ember-power-calendar-selector-years').hasAttribute('data-power-calendar-id', 'foobar', 'The attribute is bound');
   });
 });
