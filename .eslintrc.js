36bbf811ee92f6c56fbdac4d3349ccb6ad5d7558
cminick <cminick@verizonmedia.com>
2019-09-26 10:44:01 -0500

    Fix: Initial dependendency updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [
@@ -34,12 +35,13 @@ module.exports = {
       'attrs',
       'propTypes'
     ]],
-
+    'ember/no-jquery': 'error'
   },
   overrides: [
     // node files
     {
       files: [
+        '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'index.js',
@@ -55,8 +57,7 @@ module.exports = {
         'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
